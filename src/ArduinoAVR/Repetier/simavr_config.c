cb0036b39a5b4459ed38ff4826cc380dcbc02a4f
nnayo <yann_gouy@yahoo.fr>
2013-12-29 17:27:45 +0100

    add configuration for sim_avr

---
+++ src/ArduinoAVR/Repetier/simavr_config.c
@@ -0,0 +1,13 @@
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "/home/yog/TRoll/projets/simavr/simavr/sim/avr/avr_mcu_section.h"
+
+AVR_MCU(16000000, "atmega2560");
+AVR_MCU_VOLTAGES(5000, 5000, 5000);
+
+#ifdef __cplusplus
+};
+#endif
+
